b48a2c2b463dba9145a4a7b7224d81cbb887381b
Aarav2612 <89640281+Aarav2612@users.noreply.github.com>
2022-03-24 14:29:40 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "4";
+const accessCode2 = "25";
+const accessCode3 = "8";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("What is 10 divide by 2.5 ?", 100,50)
+    fill("lightblue")
+   // text("what is 10 divide by 2.5 ?", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("Square of 5", 700,150)
+    fill("lightblue")
+   // text("Hint: Performs a particular task !!", 700,170)
+
+    fill("white")
+    textSize(15)
+    text("2 + 2 mutiply 3 ?", 100,250)
+    fill("lightblue")
+   // text("Hint: Stores all information !!", 100,270)
+
+}
